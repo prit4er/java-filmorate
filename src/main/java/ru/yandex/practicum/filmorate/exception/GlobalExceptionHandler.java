68c92a019bbcf4efaf16935d1b369833177a6ec9
Aleksey Kolobov <www.kolabs@gmail.com>
2024-12-22 08:17:55 +0300

    Внес правки

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package ru.yandex.practicum.filmorate.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -20,12 +23,17 @@ public class GlobalExceptionHandler {
         return Map.of("errorMessage", "Произошла ошибка: " + e.getMessage());
     }
 
-    // Обработчик ValidationException
-    @ExceptionHandler(ValidationException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationException(final ValidationException e) {
-        log.warn("Ошибка валидации: {}", e.getMessage());
-        return Map.of("errorMessage", e.getMessage());
+    // Обработчик ошибок валидации
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 
     // Обработчик NotFoundException

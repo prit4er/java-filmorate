b0744050e20ba58fe31414963488bd49d2d26d62
Aleksey Kolobov <www.kolabs@gmail.com>
2024-12-22 07:58:44 +0300

    Внес правки

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,10 +10,10 @@ import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
     // Обработчик всех необработанных исключений
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleThrowable(final Throwable e) {
         log.error("Необработанная ошибка: ", e);
@@ -20,14 +21,16 @@ public class ExceptionHandler {
     }
 
     // Обработчик ValidationException
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationException(final ValidationException e) {
         log.warn("Ошибка валидации: {}", e.getMessage());
         return Map.of("errorMessage", e.getMessage());
     }
 
     // Обработчик NotFoundException
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleNotFoundException(final NotFoundException e) {
         log.warn("Не найден объект: {}", e.getMessage());
         return Map.of("errorMessage", e.getMessage());

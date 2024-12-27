736438ccd561d34668b143f107f377da986cee0f
Aleksey Kolobov <www.kolabs@gmail.com>
2024-12-27 20:15:39 +0300

    Вынес методы проверок в сервисы

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -9,10 +9,12 @@ import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Like;
+import ru.yandex.practicum.filmorate.model.MpaRating;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmRepository;
 import ru.yandex.practicum.filmorate.storage.GenreRepository;
 import ru.yandex.practicum.filmorate.storage.LikeRepository;
+import ru.yandex.practicum.filmorate.storage.MpaRatingRepository;
 import ru.yandex.practicum.filmorate.storage.UserRepository;
 
 import java.time.LocalDate;
@@ -31,6 +33,7 @@ public class FilmService {
     private final LikeRepository likeRepository;
     private final GenreRepository genreRepository;
     private final UserRepository userRepository;
+    private final MpaRatingRepository mpaRatingRepository;
 
     public List<FilmDto> findAll() {
         List<Film> films = filmRepository.findAll();
@@ -56,6 +59,8 @@ public class FilmService {
 
     public FilmDto create(Film film) {
         checkReleaseDate(film);
+        checkGenres(film);
+        checkRating(film);
         Film createdFilm = filmRepository.create(film);
         Map<Long, Set<Genre>> genresByFilm = genreRepository.findGenresForFilms(List.of(createdFilm.getId()));
         createdFilm.setGenres(genresByFilm.getOrDefault(createdFilm.getId(), new LinkedHashSet<>()));
@@ -63,7 +68,13 @@ public class FilmService {
     }
 
     public FilmDto update(Film film) {
+        filmRepository.findById(film.getId())
+                      .orElseThrow(() -> new NotFoundException(
+                              String.format("Фильм с таким id: %d - отсутствует", film.getId())
+                      ));
+        checkGenres(film);
         checkReleaseDate(film);
+        checkRating(film);
         Film updatedFilm = filmRepository.update(film);
         Map<Long, Set<Genre>> genresByFilm = genreRepository.findGenresForFilms(List.of(updatedFilm.getId()));
         updatedFilm.setGenres(genresByFilm.getOrDefault(updatedFilm.getId(), new LinkedHashSet<>()));
@@ -108,6 +119,25 @@ public class FilmService {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
             throw new ValidationException("Дата выхода не может быть раньше " +
                                                   "28.12.1895 - даты выхода первого в истории фильма");
+        }
+    }
+
+    private void checkGenres(Film film) {
+        for (Genre genre : film.getGenres()) {
+            // Проверка, существует ли жанр в базе
+            genreRepository.findById(genre.getId())
+                           .orElseThrow(() -> new ValidationException(
+                                   String.format("Жанр с таким id: %d - отсутствует", genre.getId())
+                           ));
+        }
+    }
+
+    private void checkRating(Film film) {
+        MpaRating mpaRating = film.getMpaRating();  // Получаем рейтинг
+        if (mpaRating == null || !mpaRatingRepository.findById(mpaRating.getId()).isPresent()) {
+            throw new ValidationException(
+                    String.format("Рейтинга с таким id: %d - нет", mpaRating.getId())
+            );
         }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmRepository.java
@@ -4,7 +4,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 
@@ -37,8 +36,6 @@ public class FilmRepository extends BaseRepository<Film> {
     }
 
     public Film create(Film film) {
-        checkRating(film);
-        checkGenre(film);
 
         long filmId = insert(
                 "INSERT INTO films(name, description, release_date, duration, rating_id) " +
@@ -55,9 +52,6 @@ public class FilmRepository extends BaseRepository<Film> {
     }
 
     public Film update(Film film) {
-        checkId(film);
-        checkRating(film);
-        checkGenre(film);
 
         update(
                 "UPDATE films " +
@@ -84,47 +78,6 @@ public class FilmRepository extends BaseRepository<Film> {
                     "INSERT INTO film_genres(film_id, genre_id) VALUES (?, ?)",
                     film.getId(),
                     genre.getId()
-            );
-        }
-    }
-
-    private void checkRating(Film film) {
-        Integer count = jdbc.queryForObject(
-                "SELECT COUNT(*) FROM ratings WHERE id = ?",
-                Integer.class,
-                film.getMpaRating().getId()
-        );
-        if (count == 0) {
-            throw new ValidationException(
-                    String.format("Рейтинг с таким id: %d - отсутствует", film.getMpaRating().getId())
-            );
-        }
-    }
-
-    private void checkGenre(Film film) {
-        for (Genre genre : film.getGenres()) {
-            Integer count = jdbc.queryForObject(
-                    "SELECT COUNT(*) FROM genres WHERE id = ?",
-                    Integer.class,
-                    genre.getId()
-            );
-            if (count == 0) {
-                throw new ValidationException(
-                        String.format("Жанр с таким id: %d - отсутствует", genre.getId())
-                );
-            }
-        }
-    }
-
-    private void checkId(Film film) {
-        Integer count = jdbc.queryForObject(
-                "SELECT COUNT(*) FROM films WHERE id = ?",
-                Integer.class,
-                film.getId()
-        );
-        if (count == 0) {
-            throw new NotFoundException(
-                    String.format("Фильм с таким id: %d - отсутствует", film.getId())
             );
         }
     }
